4f8e574d20ddb4de161e991f4668217f24d41ea1
karuppasamythiyagu <rjanani671@gmail.com>
2021-11-10 14:12:22 +0530

    some project will added

---
+++ src/org/sample/SampleTest.java
@@ -0,0 +1,14 @@
+package org.sample;
+
+public class SampleTest {
+
+	private void StudentName() {
+		System.out.println("Student Name:Karan");
+	}
+	
+	private void studentId() {
+		System.out.println("Student Id:ME1234");
+
+	}
+
+}
